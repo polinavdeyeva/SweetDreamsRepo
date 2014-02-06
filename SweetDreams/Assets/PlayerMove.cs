bb6537416c68e547ebde81cb64ea5109b5ba919e
ManvierOnWheelz <metaleagle777@gmail.com>
2014-02-06 09:56:51 -0500

    Added and fixed player animations

--- SweetDreams/Assets/PlayerMove.cs
+++ SweetDreams/Assets/PlayerMove.cs
@@ -7,27 +7,36 @@ public class PlayerMove : MonoBehaviour {
 	Vector3 moveDirection = new Vector3(0f, 0f, 0f);
 	protected CharacterController controller;
 	public float gravity = -9.8f;
-	protected float jumpHeight = 2.7f;
+	protected float jumpHeight = 2f;
 	public float terminalVelocity = -1f;
 	Vector3 variableLeft = new Vector3();
 	Vector3 variableRight = new Vector3();
 	Vector3 screenDim = new Vector3();
 	float HorizontalMovement;
 	float edgeOffset = 5.0f;
+	Animator animator;
+	public float defaulty;
 
 	// Use this for initialization
 	void Start () {
 		controller = transform.GetComponent<CharacterController>();
 		enabled = true;
+		animator = transform.GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
 	public virtual void Update () {
 		UpdateScreenVars ();
 		if(moveDirection.y > terminalVelocity){
+			defaulty = moveDirection.y;
 			moveDirection.y += gravity *Time.deltaTime;
+			if(moveDirection.y > defaulty){
+				animator.SetTrigger("startJump");
+			}
 		}
+
 		controller.Move (moveDirection);
+
 	
 	}
 	public void UpdateScreenVars(){
@@ -44,11 +53,14 @@ public class PlayerMove : MonoBehaviour {
 		HorizontalMovement = Input.GetAxis (horizontalAxis);
 		if (transform.localPosition.x < variableRight.x - edgeOffset && HorizontalMovement > 0) {
 			moveDirection.x = HorizontalMovement * Time.deltaTime * speed;
+			animator.SetBool("isWalking", true);
 		}
 		else if(transform.localPosition.x > variableLeft.x + edgeOffset && HorizontalMovement < 0){
 			moveDirection.x = HorizontalMovement * Time.deltaTime * speed;
+			animator.SetBool("isWalking", true);
 		}
 		else {
+			animator.SetBool("isWalking", false);
 			moveDirection.x = 0;
 		}
 		if(Input.GetButtonDown(jumpButton)){
