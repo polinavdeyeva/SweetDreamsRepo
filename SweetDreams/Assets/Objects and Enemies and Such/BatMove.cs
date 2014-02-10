dbabc00b0f06bcad3b03f42eb3152343a0bcbb01
unknown <jpalmieri@US-507-04.amsacs.org>
2014-02-10 08:38:37 -0500

    Added a bat + made prefabs for bat and game over screen

---
+++ SweetDreams/Assets/Objects and Enemies and Such/BatMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatMove : MonoBehaviour {
+	public Transform prefab;
+	GameObject P1;
+	GameObject P2;
+	Vector3 pos = new Vector3(); 
+	
+	// Use this for initialization
+	void Start () {
+		P1 = GameObject.Find("WarriorWomanParent");
+		P2 = GameObject.Find ("WonderWomanParent");
+		pos = transform.localPosition;
+		pos.y = 20f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if((transform.localPosition.x - 6f) <= (P1.transform.localPosition.x) || (transform.localPosition.x - 6f) <= (P2.transform.localPosition.x)){
+			pos.x -= 0.5f;
+			pos.y -= 0.5f;
+		}
+		transform.localPosition = pos;*/
+		if(pos.y < 20f && pos.y > 5f){
+			pos.y -= 0.1f;
+		}
+		if(pos.y <= 20f && pos.y >= 5f){
+			pos.y += 0.1f;
+		}
+		transform.localPosition = pos;
+
+		
+		
+	}
+	void OnTriggerEnter(Collider other){
+		PlayerMove Pove = other.gameObject.GetComponent<PlayerMove>();
+		Pove.RemoveHealth ();
+	}
+}
